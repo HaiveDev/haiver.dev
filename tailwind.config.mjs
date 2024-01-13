c35724194406c08800b29ee3ab802db2318e0463
Haiver Rueda <devhaiverueda@gmail.com>
2024-01-12 23:24:04 -0500

    edited fontFamily

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,9 +4,8 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                'poppins': ['poppins', 'sans-serif'],
                 'Megrim' : ['Megrim','sans-serif'],
-                'Averia' : ['Averia Sans Libre','sans-serif']
+                'Averia' : ['Averia Sans Libre','sans-serif'],
             },
         },
     },
